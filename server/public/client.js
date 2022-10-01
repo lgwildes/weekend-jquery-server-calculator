013d58b1fad3f36af4894bb4b8bd9561432db0a1
Lydia Wildes <lydiawildes1@gmail.com>
2022-10-01 14:45:04 -0500

    create post endpoint and work on calculate functions

--- server/public/client.js
+++ server/public/client.js
@@ -7,12 +7,74 @@ let operator = '';
 function onReady() {
     console.log(`let's do some math! 🤓`)
 
-    $('#calculatorForm').on('submit', showResult)
-    
+//***TODO create loadPreviousCalculations function and call it onReady.***//
+
+    $('#calculatorForm').on('submit', calculate,)
+
+    //event handlers for each mathematical operator
+    $('#add').on('click', addOperator)
+    $('#subtract').on('click', subtractOperator)
+    $('#multiply').on('click', multiplyOperator)
+    $('#divide').on('click', divideOperator)
 
 }
+// these functions set 'operator' to whatever button was clicked
+function addOperator() {
+    operator = '+'
+    console.log('testing operator, expect add', operator)
+}
 
-function showResult(evt) {
+function subtractOperator() {
+    operator = '-'
+    console.log('testing operator, expect subtract', operator)
+}
+
+function multiplyOperator() {
+    operator = '*'
+    console.log('testing operator, expect  multiply', operator)
+}
+
+function divideOperator() {
+    operator = '/'
+    console.log('testing oprator, expect divide', operator)
+}
+
+
+
+// send post request to server
+function calculate(evt){
     evt.preventDefault();
-    $('#results').append(`result`)
+    //this is the object we send to server
+    calculation = {
+        firstInput: $('#firstInput').val(),
+        secondInput: $('#secondInput').val(),
+        operator: operator // value is set based on button clicked
+
+    }
+    console.log ('testing calculation object',calculation )
+
+    $.ajax({
+        url:'/calculate',
+        method: 'POST',
+        data: calculation
+    })
+
+    .then((response) => {
+            console.log('POST /calculate response expect status', response);
+    })
+    .catch(err => {
+            console.log('POST /calculate error', err);
+    });
+
+    // render() 
+
+
+
+
 }
+
+//TODO 
+// function render() {
+
+//     $('#results').append(`result`)
+// }
--- server/server.js
+++ server/server.js
@@ -7,6 +7,38 @@ app.use(express.static('server/public'));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+// global variable to hold result of calculation
+let result = ''
+
+//POST calculate end point
+app.post('/calculate', (req, res) => {          //req.body = calculation object
+                                                  //{firstInput: , secondInput: , operator: ,}
+  console.log('client has requested a calculation 📓', req.body);
+
+  // figure out what function to call based on operator value
+
+
+  //call function to do calculation
+  calculate();
+
+  res.sendStatus(201)
+})
+
+
+
+function calculate(input1, input2,) {
+  input1 * input2
+  return result
+
+}
+
+
+
+
+
+
+
+
 app.listen(port, () => {
   console.log('listening on port', port);
 });
