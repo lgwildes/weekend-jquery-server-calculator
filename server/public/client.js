639708633b99838c1b261f960af55ce751949093
Lydia Wildes <lydiawildes1@gmail.com>
2022-10-01 13:49:41 -0500

    create onReady and showResult functions

--- server/public/client.js
+++ server/public/client.js
@@ -2,8 +2,17 @@ console.log('in client.js');
 
 $(document).ready(onReady);
 
+let operator = '';
+
 function onReady() {
     console.log(`let's do some math! 🤓`)
 
+    $('#calculatorForm').on('submit', showResult)
     
+
+}
+
+function showResult(evt) {
+    evt.preventDefault();
+    $('#results').append(`result`)
 }
